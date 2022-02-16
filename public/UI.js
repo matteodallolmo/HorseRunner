447204cf6bbc98cdcadfa6efbfb720b5bf880cae
arianaazarbal <arianaazarbal@icloud.com>
2022-02-16 10:15:05 -0800

    final edits

--- public/UI.js
+++ public/UI.js
@@ -1,13 +1,14 @@
 class UI
 {
-  let currentLevel;
+  //let currentLevel;
   constructor()
   {
+    this.currentLevel=0;
     document.body.style.backgroundImage = "url('TitleCard.png')";
     document.body.style.backgroundRepeat = "no-repeat";
     document.body.style.backgroundSize = "cover";
-    currentLevel = new Level (1);
-    checkGameState();
+    //currentLevel = new Level (1);
+    //currentLevel.checkGameState();
   }
 
 
