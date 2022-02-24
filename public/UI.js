5c4010510f4b1c2bfdb12d28a9a6a1a7ec9a9576
videozane <zanedanton@gmail.com>
2022-02-23 19:19:31 -0800

    Added Scoreboard class

---
+++ public/UI.js
@@ -0,0 +1,13 @@
+class UI
+{
+  //let currentLevel;
+  constructor()
+  {
+    this.currentLevel=0;
+    document.body.style.backgroundImage = "url('TitleCard.png')";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "cover";
+    //currentLevel = new Level (1);
+    //currentLevel.checkGameState();
+  }
+}
