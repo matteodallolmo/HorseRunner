7afb4a75e7bb16483b1076c16f3c035e7593b073
Matteo Dall'Olmo <matteo.dallolmo1@gmail.com>
2022-02-26 12:41:57 -0800

    made progress on animation methods

--- public/Horse.js
+++ public/Horse.js
@@ -1,6 +1,14 @@
 
 class Horse{
-  constructor(lanePos,sprite){
+  constructor(lanePos,levelNum)//level 0 for PlayerHorse
+  {
+    this.spriteArray = [
+      "Sprites/horseSprite.png",
+      "Sprites/turtleSprite.png",
+      "Sprites/camelSprite.png",
+      "Sprites/zebraSprite.png",
+      "Sprites/cheetahSprite.png"];
+
     this.position = 0;
     this.lanePosition = lanePos;//the verticle position of the Horse when not jumping
     this.spriteLength = 197; //width of sprite
@@ -9,7 +17,12 @@ class Horse{
     this.isJumping = false; // stores whether the horse is jumping
     this.velocity = 0; // velocity of the horse
     this.acceleration = 0; // acceleration of the horse
-    this.spriteSheet=sprite; // sprite sheet of the horse, animate by css
+    this.spriteSheet = this.spriteArray[levelNum]; // sprite sheet of the horse, animate by css
+    this.img = new Image();
+    this.img.src = this.spriteSheet;
+
+    this.cycleLoop = [0, 1, 2, 3, 4, 5];
+    this.currentLoopIndex = 0;
   }
   movement(time){//move expected amount of space over the refresh time
   //run every frame when enabled
@@ -32,8 +45,36 @@ class Horse{
     setTimeout(()=>{this.recentlyJumped=false},500);
     //someone has to figure out the animation here.
   }
-  animate(velocity){
-    //sets animation-duration of sprite(css) to 6*Velocity, animation-timing-function to steps(6). Used every frame.
-    //someone else needs to complete this.
+
+  drawFrame(canvas, ctx, frameNum, canvasX)
+  {
+    const scale = 1;
+    const width = 164;
+    const height = 93;
+    const startXPos = 38;
+    const startYPos = 24;
+
+    ctx.drawImage(this.img, startXPos+(width+30)*frameNum, startYPos, width, height, canvasX, this.lanePosition, width*scale, height*scale);
   }
+
+  animate(keyFrame, velocity, spriteSize)
+    {
+    let canvas = document.querySelector('canvas');
+    let ctx = canvas.getContext('2d');
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    this.drawFrame(canvas, ctx, this.cycleLoop[this.currentLoopIndex], 0);
+
+    this.currentLoopIndex++;
+
+    if (this.currentLoopIndex >= this.cycleLoop.length) {
+        this.currentLoopIndex = 0;
+    }
+    requestAnimationFrame(this.animate);
+  }
+
+  initializeAnimation() {
+    requestAnimationFrame(this.animate);
+  }
+
 }
