6b2eba4fe4f08751289def580ec7a4c2ccb00955
arianaazarbal <arianaazarbal@icloud.com>
2022-02-28 12:43:58 -0800

    updated methods in level, added instance variables to playerHorse and roboHorse

--- public/Level.js
+++ public/Level.js
@@ -3,13 +3,18 @@
 
 class Level {
   constructor(levelNumber) {
-    this.obstacles = [2000];
+    this.obstacles = new Array (2000);
     this.levelNumber = levelNumber;
+    this.winner = null;
+    this.isFinished;
     this.player = new PlayerHorse(1,1);//temp parameters
+    this.initialTime = Date.now;
+    this.horsePerLane =  [new RoboHorse (this.levelNumber), new RoboHorse (this.levelNumber), this.player, new RoboHorse (this.levelNumber), new RoboHorse (this.levelNumber), new RoboHorse (this.levelNumber)]
   }
 
   checkGameState() {
-    let playerPos = this.player.position;
+    let playerPos = this.player.getPosition();
+    console.log (playerPos);
     if (this.player.isJumping) {
       if (obstacles[playerPos] == true) {
           return 1;
@@ -19,6 +24,54 @@ class Level {
         return 2;
       }
       return 0;
-
     }
+
+    checkForRoboHorseFinish()
+    {
+      for (var x = 0; x<5; x++)
+      {
+        if (x!= 3)
+        {
+          if (this.horsePerLane[x].getPosition() ==2000)
+          {
+            currentTime = Date.now;
+            this.horsePerLane[x].setRaceTime (currentTime-initialTime);
+            if (this.winner == null)
+            {
+              this.winner = this.horsePerLane[x];
+            }
+          }
+        }
+      }
+    }
+
+    isFinished()
+    {
+      for (var x = 0; x<5; x++)
+      {
+        if (this.horsePerLane[x].getTime() != null)
+        {
+        }
+        else {
+          return false;
+        }
+        return true;
+      }
+    }
+
+    isWinner()
+    {
+      if (this.winner = this.player)
+      {
+        return true;
+      }
+      return false;
+    }
+
+
+    handleCollision()
+    {
+      this.player.set
+    }
+
   }
--- public/UI.js
+++ public/UI.js
@@ -54,14 +54,14 @@ class UI{
 
     ctx.save();
     ctx.lineWidth = 6;
-    this.currentLevel.display(ctx);
+    //this.currentLevel.display(ctx);
     ctx.restore();
 
-    window.requestAnimationFrame(this.draw);
+    window.requestAnimationFrame(this.draw());
   }
 
   initializeAnimation() {
-    window.requestAnimationFrame(this.draw);
+    window.requestAnimationFrame(this.draw());
   }
 
 
