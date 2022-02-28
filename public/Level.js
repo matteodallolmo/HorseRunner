3872f0d9693cdc1dc344cb1a6727c4d33f13d1a7
Alex-Shane <ashane1230@gmail.com>
2022-02-28 10:14:04 -0800

    finished constructor

--- public/Level.js
+++ public/Level.js
@@ -3,6 +3,11 @@
 
 class Level {
   constructor(levelNumber) {
+    this.refreshTime = 60; //fps
+    this.obstaclesArray = [];
+    this.horsesArray = [];
+    this.backgroundImage = new Image();// Create new img element
+    this.img.src = 'CompleteTrack.png'; // Set source path
     this.obstacles = [2000];
     this.levelNumber = levelNumber;
     this.player = new PlayerHorse(1,1);//temp parameters
