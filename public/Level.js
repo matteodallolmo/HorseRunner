a3bc19002619b80a4ed35f4d49897fb7eb585adc
Alex-Shane <ashane1230@gmail.com>
2022-02-28 10:43:49 -0800

    populateObstacleArray completed (graphics are still needed)

--- public/Level.js
+++ public/Level.js
@@ -3,14 +3,71 @@
 
 class Level {
   constructor(levelNumber) {
+    this.levelNum = levelNumber;
     this.refreshTime = 60; //fps
     this.obstaclesArray = [];
     this.horsesArray = [];
+    this.obstacleIndexes = [];
     this.backgroundImage = new Image();// Create new img element
     this.img.src = 'CompleteTrack.png'; // Set source path
-    this.obstacles = [2000];
+    this.timer = null;
+    this.obstacles = [];
+    this.obstacles.length = 20000;
     this.levelNumber = levelNumber;
     this.player = new PlayerHorse(1,1);//temp parameters
+  }
+
+  getRandomInt(min, max)
+  {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  }
+
+  populateObstaclesArray ()
+  {
+    if (this.levelNum == 1)
+    {
+      generateIndexesForObstaclesToBePlaced (4);
+    }
+    else if (this.levelNum == 2)
+    {
+      generateIndexesForObstaclesToBePlaced (6);
+    }
+    else if (this.levelNum == 3)
+    {
+      generateIndexesForObstaclesToBePlaced (8);
+    }
+    else {
+      generateIndexesForObstaclesToBePlaced(12);
+    }
+    let obstaclesPutIn = 0;
+    for (int k = 0; k < 20000; k++)
+    {
+      if (k == obstacleIndexes[obstaclesPutIn])
+      {
+        obstaclesArray [k] = true;
+        obstaclesPutIn++;
+      }
+    }
+    obstacleIndexes = [];
+  }
+
+  generateIndexesForObstaclesToBePlaced (numObstacles)
+  {
+    for (int k = 0; k < numObstacles; k++)
+    {
+      let num = this.getRandomInt(0,20000);
+      while (k != 0 && this.obstacleIndexes[k].includes(num))
+      {
+        num = this.getRandomInt(0,20000);
+      }
+      this.obstacleIndexes[k] = num;
+    }
+  }
+  playGame()
+  {
+
   }
 
   checkGameState() {
