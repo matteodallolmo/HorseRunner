1360c9ee7cf40eff5408d35548a3fd4cebc9b6da
Matteo Dall'Olmo <matteo.dallolmo1@gmail.com>
2022-03-21 08:30:54 -0700

    fixed jaydon's changes

--- public/playerHorse.js
+++ public/playerHorse.js
@@ -1,55 +1,53 @@
-//
-//
-// class PlayerHorse extends Horse
-// {
-//   constructor(lanePos, levelNum) {
-//     super(lanePos, levelNum);
-//     this.accelToAdd = 0;
-//     this.prevKey = 0;
-//     this.currentKey = 0;
-//     this.raceTime = null;
-//   }
-//
-//
-//   //this takes in a INT that represents the key
-//   //37 = leftArrow, 39 = rightArrow
-//   getPosition()
-//   {
-//     return super.position;
-//   }
-//   setRaceTime(finishedTime)
-//   {
-//     this.raceTime = finsishedTime;
-//   }
-//   getRaceTime()
-//   {
-//     return this.raceTime;
-//   }
-//   keyPressed(pressedKey)
-//   {
-//     this.currentKey = pressedKey;
-//     if(this.currentKey == 37 || this.currentKey == 39)
-//     {
-//       if(this.currentKey != this.prevKey)
-//       {
-//         this.accelToAdd += 0.05;
-//         this.prevKey = this.currentKey;
-//       }
-//       else
-//       {
-//         this.acceleration = 0;
-//         this.velocity -= 1;
-//       }
-//     }
-//     if(pressedKey == 32)//32 is keycode for spacebar
-//     {
-//       jump()
-//     }
-//   }
-//
-//   updateAccel()
-//   {
-//     this.acceleration = this.acceleration+this.accelToAdd;
-//     this.accelToAdd = 0;
-//   }
-// }
+class PlayerHorse extends Horse
+{
+  constructor(lanePos, levelNum) {
+    super(lanePos, levelNum);
+    this.accelToAdd = 0;
+    this.prevKey = 0;
+    this.currentKey = 0;
+    this.raceTime = null;
+  }
+
+
+  //this takes in a INT that represents the key
+  //37 = leftArrow, 39 = rightArrow
+  getPosition()
+  {
+    return super.position;
+  }
+  setRaceTime(finishedTime)
+  {
+    this.raceTime = finsishedTime;
+  }
+  getRaceTime()
+  {
+    return this.raceTime;
+  }
+  keyPressed(pressedKey)
+  {
+    this.currentKey = pressedKey;
+    if(this.currentKey == 37 || this.currentKey == 39)
+    {
+      if(this.currentKey != this.prevKey)
+      {
+        this.accelToAdd += 0.05;
+        this.prevKey = this.currentKey;
+      }
+      else
+      {
+        this.acceleration = 0;
+        this.velocity -= 1;
+      }
+    }
+    if(pressedKey == 32)//32 is keycode for spacebar
+    {
+      jump()
+    }
+  }
+
+  updateAccel()
+  {
+    this.acceleration = this.acceleration+this.accelToAdd;
+    this.accelToAdd = 0;
+  }
+}
--- public/roboHorse.js
+++ public/roboHorse.js
@@ -1,80 +1,77 @@
-//
-//
-//
-// class RoboHorse extends Horse{
-//
-//   constructor (lanePos, levelNum)
-//   {
-//     super(lanePos, levelNum);
-//     this.lanePosition = lanePos;
-//     this.level = levelNum;
-//     this.raceTime = null;
-//     this.isJumping = true;
-//     let possibleNames = [];
-//     this.maxRoboHorseVelocities = [10,20,30,70];
-//     this.velocity = 0;
-//     this.acceleration = 0;
-//
-//     if (levelNum == 1)
-//     {
-//         this.possibleNames = ["Rafael", "Donnatello", "Leonardo", "Shelly", "Mr. Green", "Tuck", "Franklin", "Michaelangelo"];
-//     }
-//     else if (levelNum == 2)
-//     {
-//         this.possibleNames = ["Desert", "Humpy", "Dehydrated", "Lawrence", "Mohammad"];
-//     }
-//     else if (levelNum == 3)
-//     {
-//         this.possibleNames = ["Cookie", "Miss Zebra", "Savannah", "Speedy", "Jordan"];
-//     }
-//     else
-//     {
-//         this.possibleNames = ["Dash"];
-//     }
-//     let name = this.generateName();
-//   }
-//   getPosition()
-//   {
-//     return super.position;
-//   }
-//   setRaceTime(finishedTime)
-//   {
-//     this.raceTime = finsishedTime;
-//   }
-//   getRaceTime ()
-//   {
-//     return this.raceTime;
-//   }
-//   getRandomInt(min, max)
-//   {
-//     min = Math.ceil(min);
-//     max = Math.floor(max);
-//     return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-//   }
-//   generateName ()
-//   {
-//        if (this.level == 4)
-//        {
-//          return "Dash";
-//        }
-//        else
-//        {
-//          let numOfNames = this.possibleNames.length;
-//          let randNum = this.getRandomInt (0, numOfNames);
-//          return this.possibleNames[randNum];
-//        }
-//   }
-//   updateAcceleration()
-//   {
-//     this.maxVelo = this.maxRoboHorseVelocities [this.level-1];
-//     if (this.velocity < this.maxVelo)
-//     {
-//         this.acceleration += Math.random() * .08 +.02;
-//         console.log ("New Acceleration is " + this.acceleration);
-//     }
-//     else
-//     {
-//         console.log ("max velo already reached");
-//     }
-//   }
-// }
+class RoboHorse extends Horse {
+
+  constructor (lanePos, levelNum)
+  {
+    super(lanePos, levelNum);
+    this.lanePosition = lanePos;
+    this.level = levelNum;
+    this.raceTime = null;
+    this.isJumping = true;
+    let possibleNames = [];
+    this.maxRoboHorseVelocities = [10,20,30,70];
+    this.velocity = 0;
+    this.acceleration = 0;
+
+    if (levelNum == 1)
+    {
+        this.possibleNames = ["Rafael", "Donnatello", "Leonardo", "Shelly", "Mr. Green", "Tuck", "Franklin", "Michaelangelo"];
+    }
+    else if (levelNum == 2)
+    {
+        this.possibleNames = ["Desert", "Humpy", "Dehydrated", "Lawrence", "Mohammad"];
+    }
+    else if (levelNum == 3)
+    {
+        this.possibleNames = ["Cookie", "Miss Zebra", "Savannah", "Speedy", "Jordan"];
+    }
+    else
+    {
+        this.possibleNames = ["Dash"];
+    }
+    let name = this.generateName();
+  }
+  getPosition()
+  {
+    return super.position;
+  }
+  setRaceTime(finishedTime)
+  {
+    this.raceTime = finsishedTime;
+  }
+  getRaceTime ()
+  {
+    return this.raceTime;
+  }
+  getRandomInt(min, max)
+  {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  }
+  generateName ()
+  {
+       if (this.level == 4)
+       {
+         return "Dash";
+       }
+       else
+       {
+         let numOfNames = this.possibleNames.length;
+         let randNum = this.getRandomInt (0, numOfNames);
+         return this.possibleNames[randNum];
+       }
+  }
+  updateAcceleration()
+  {
+    this.maxVelo = this.maxRoboHorseVelocities [this.level-1];
+    if (this.velocity < this.maxVelo)
+    {
+        this.acceleration += Math.random() * .08 +.02;
+        console.log ("New Acceleration is " + this.acceleration);
+    }
+    else
+    {
+        console.log ("max velo already reached");
+    }
+  }
+}
